c3ef2deaded70a67d0d49df115ac7bcc06ff96e7
Grégoire Grison <46027716+Ggrison@users.noreply.github.com>
2018-12-20 21:44:32 +0100

    Add files via upload

--- robtex-api/robtex-api.py
+++ robtex-api/robtex-api.py
@@ -6,10 +6,13 @@ import requests
 URL = "https://freeapi.robtex.com/ipquery/"
 IpRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
 
+def write(file, txt):
+    f.write(txt + "\n")
+    print(txt.replace(";"," ; "))
+
 if len(sys.argv) == 2:
     f = open("robtex-api-result.csv", "w")
-    f.write("IP;Country;City;Description;Route\n")
-    print("IP ; Country ; City ; Description;Route")
+    write(f, "IP;Country;City;Description;Route")
     for ip in open(sys.argv[1]):
         ip = ip.replace("\n" ,"")
         if re.match(IpRegex, ip):
@@ -20,11 +23,11 @@ if len(sys.argv) == 2:
                 descr = r.json()['whoisdesc'] if 'whoisdesc' in r.json() else "N/A"
                 route = r.json()['bgproute'] if 'bgproute' in r.json() else "N/A"
                 moreInfo = "https://www.robtex.com/ip-lookup/" + ip + "#records"
-                f.write(ip + ";" + country + ";" + city + ";" + descr + ";" + route + ";" + moreInfo + "\n")
-                print(ip, ";", country, ";", city, " ; ", descr, ";", route, ";", moreInfo)
+                write(f, ip + ";" + country + ";" + city + ";" + descr + ";" + route + ";" + moreInfo)
+            else:
+                write(f, "HTTP error code: " + str(r.status_code) + " while checking for ip: " + ip)
         else:
-            f.write("Invalid IP format:" + ip + "\n")
-            print("Invalid IP format:", ip)
+            write(f, "Invalid IP format: " + ip)
     f.close()
 else:
     print("Usage: robtex-api.py \"IP_file.txt\"")
